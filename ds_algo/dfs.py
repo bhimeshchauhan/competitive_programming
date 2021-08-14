8445a75ae7543c3c2b6cd5a20a4c8c06127f12c0
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-08-14 21:26:40 +0530

    bst

---
+++ ds_algo/dfs.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val
+        self.right = right
+        self.left = left
+        
+
+
+class BST:
+    def __init__(self) -> None:
+        
+        
