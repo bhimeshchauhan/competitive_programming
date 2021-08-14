5bdc97d13bc60f3c7f52e63c4d9fa336d2b52583
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-08-14 22:20:33 +0530

    dfs-kth-sum

---
+++ scripts/fb/kthSmallestElementBST.py
@@ -0,0 +1,31 @@
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        def dfs(currentNode):
+            return dfs(currentNode.left) + [currentNode.val] + dfs(currentNode.right) if currentNode else []
+        return dfs(root)[k-1]
