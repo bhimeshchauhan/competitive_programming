01df239a60d4bf370ae7c79f9f608e183df07dc1
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-08-15 20:25:44 +0530

    valid-palindrome

---
+++ scripts/fb/validPalindrome.py
@@ -0,0 +1,45 @@
+"""
+
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+
+"""
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.strip()
+        print(s)
+        
+        start = 0
+        end = len(s)-1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+                continue
+            if not s[end].isalnum():
+                end -= 1
+                continue
+            if s[start].lower() != s[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
+                
