028bb8ee665fb7049f50070d36d3a9cd03eaca77
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-08-22 13:18:18 +0530

    update-array-sum-eq-x

--- scripts/leetcode/fb/subArraySumEqualK.py
+++ scripts/leetcode/fb/subArraySumEqualK.py
@@ -28,10 +28,8 @@ class Solution:
         sums = 0
         d = dict()
         d[0] = 1
-        
         for i in range(len(nums)):
             sums += nums[i]
-            print(i, d, sums, k, count)
             count += d.get(sums-k,0)
             d[sums] = d.get(sums,0) + 1
         
