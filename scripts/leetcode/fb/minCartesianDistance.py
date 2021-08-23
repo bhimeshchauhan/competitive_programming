3f9245f39d5f0e33a4a8f34df2ef1314efe4a539
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-08-23 22:15:36 +0530

    cartesian-coord

--- scripts/leetcode/fb/minCartesianDistance.py
+++ scripts/leetcode/fb/minCartesianDistance.py
@@ -35,8 +35,10 @@ Constraints:
 
 """
 import heapq
+
+
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    def kClosest(self, points, k):
         heap = []
         for (x, y) in points:
             dist = -(x*x + y*y)
@@ -44,5 +46,5 @@ class Solution:
                 heapq.heappushpop(heap, (dist, x, y))
             else:
                 heapq.heappush(heap, (dist, x, y))
-        
-        return [(x,y) for (dist,x, y) in heap]
+
+        return [(x, y) for (dist, x, y) in heap]
