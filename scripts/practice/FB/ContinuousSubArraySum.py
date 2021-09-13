010ae2401d869cd9e7455e8b25b0ff78043357d2
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-13 17:23:42 +0530

    Continuous Subarray Sum

--- scripts/practice/FB/ContinuousSubArraySum.py
+++ scripts/practice/FB/ContinuousSubArraySum.py
@@ -2,7 +2,7 @@
 
 Continuous Subarray Sum
 
-Given an integer array nums and an integer k, return true if nums has a continuous subarray of size at 
+Given an integer array nums and an integer k, return true if nums has a continuous subarray of size at
 least two whose elements sum up to a multiple of k, or false otherwise.
 
 An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
@@ -24,7 +24,7 @@ Example 3:
 
 Input: nums = [23,2,6,4,7], k = 13
 Output: false
- 
+
 
 Constraints:
 
@@ -39,7 +39,7 @@ Constraints:
 
 class Solution:
     def checkSubarraySum(self, nums, k):
-         def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+         def checkSubarraySum(self, nums, k):
         d = {0:-1}
         sums = 0
 
