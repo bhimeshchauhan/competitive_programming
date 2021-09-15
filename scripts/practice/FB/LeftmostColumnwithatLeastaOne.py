c666794290ce0c63b533397ef19ea293f2e7bc8a
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-16 02:30:25 +0530

    Leftmost Column with at Least a One

--- scripts/practice/FB/LeftmostColumnwithatLeastaOne.py
+++ scripts/practice/FB/LeftmostColumnwithatLeastaOne.py
@@ -71,3 +71,28 @@ class Solution:
         
         # If we never left the last column, it must have been all 0's.
         return current_col + 1 if current_col != cols - 1 else -1
+    
+    
+
+
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        rows, cols = binaryMatrix.dimensions()
+        smallest_index = cols
+        for row in range(rows):
+            # Binary Search for the first 1 in the row.
+            lo = 0
+            hi = cols - 1
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if binaryMatrix.get(row, mid) == 0:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            # If the last element in the search space is a 1, then this row
+            # contained a 1.
+            if binaryMatrix.get(row, lo) == 1:
+                smallest_index = min(smallest_index, lo)
+        # If smallest_index is still set to cols, then there were no 1's in 
+        # the grid. 
+        return -1 if smallest_index == cols else smallest_index
