398de3259440e43647efb1db9db4bfeb39df60e9
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-17 23:49:36 +0530

    Course Schedule

---
+++ scripts/practice/FB-reRun/CanAllCourseBeTaken.py
@@ -0,0 +1,186 @@
+"""
+
+Course Schedule
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+You are given an array prerequisites where prerequisites[i] = [ai, bi] 
+indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+Constraints:
+
+1 <= numCourses <= 105
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+All the pairs prerequisites[i] are unique.
+
+"""
+
+# Backtracking
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        from collections import defaultdict
+        courseDict = defaultdict(list)
+
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            courseDict[prevCourse].append(nextCourse)
+
+        path = [False] * numCourses
+        for currCourse in range(numCourses):
+            if self.isCyclic(currCourse, courseDict, path):
+                return False
+        return True
+
+
+    def isCyclic(self, currCourse, courseDict, path):
+        """
+        backtracking method to check that no cycle would be formed starting from currCourse
+        """
+        if path[currCourse]:
+            # come across a previously visited node, i.e. detect the cycle
+            return True
+
+        # before backtracking, mark the node in the path
+        path[currCourse] = True
+
+        # backtracking
+        ret = False
+        for child in courseDict[currCourse]:
+            ret = self.isCyclic(child, courseDict, path)
+            if ret: break
+
+        # after backtracking, remove the node from the path
+        path[currCourse] = False
+        return ret
+
+
+# Postorder DFS
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        from collections import defaultdict
+        courseDict = defaultdict(list)
+
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            courseDict[prevCourse].append(nextCourse)
+
+        checked = [False] * numCourses
+        path = [False] * numCourses
+
+        for currCourse in range(numCourses):
+            if self.isCyclic(currCourse, courseDict, checked, path):
+                return False
+        return True
+
+
+    def isCyclic(self, currCourse, courseDict, checked, path):
+        """   """
+        # 1). bottom-cases
+        if checked[currCourse]:
+            # this node has been checked, no cycle would be formed with this node.
+            return False
+        if path[currCourse]:
+            # came across a marked node in the path, cyclic !
+            return True
+
+        # 2). postorder DFS on the children nodes
+        # mark the node in the path
+        path[currCourse] = True
+
+        ret = False
+        # postorder DFS, to visit all its children first.
+        for child in courseDict[currCourse]:
+            ret = self.isCyclic(child, courseDict, checked, path)
+            if ret: break
+
+        # 3). after the visits of children, we come back to process the node itself
+        # remove the node from the path
+        path[currCourse] = False
+
+        # Now that we've visited the nodes in the downstream,
+        #   we complete the check of this node.
+        checked[currCourse] = True
+        return ret
+    
+
+# Topological Sort
+
+class GNode(object):
+    """  data structure represent a vertex in the graph."""
+    def __init__(self):
+        self.inDegrees = 0
+        self.outNodes = []
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        from collections import defaultdict, deque
+        # key: index of node; value: GNode
+        graph = defaultdict(GNode)
+
+        totalDeps = 0
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            graph[prevCourse].outNodes.append(nextCourse)
+            graph[nextCourse].inDegrees += 1
+            totalDeps += 1
+
+        # we start from courses that have no prerequisites.
+        # we could use either set, stack or queue to keep track of courses with no dependence.
+        nodepCourses = deque()
+        for index, node in graph.items():
+            if node.inDegrees == 0:
+                nodepCourses.append(index)
+
+        removedEdges = 0
+        while nodepCourses:
+            # pop out course without dependency
+            course = nodepCourses.pop()
+
+            # remove its outgoing edges one by one
+            for nextCourse in graph[course].outNodes:
+                graph[nextCourse].inDegrees -= 1
+                removedEdges += 1
+                # while removing edges, we might discover new courses with prerequisites removed, i.e. new courses without prerequisites.
+                if graph[nextCourse].inDegrees == 0:
+                    nodepCourses.append(nextCourse)
+
+        if removedEdges == totalDeps:
+            return True
+        else:
+            # if there are still some edges left, then there exist some cycles
+            # Due to the dead-lock (dependencies), we cannot remove the cyclic edges
+            return False
