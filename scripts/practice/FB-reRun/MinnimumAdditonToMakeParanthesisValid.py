0c14c7546e49b90ccdedad246e8b4cd91ddd2316
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-18 17:26:37 +0530

    Minimum Add to Make Parentheses Valid

---
+++ scripts/practice/FB-reRun/MinnimumAdditonToMakeParanthesisValid.py
@@ -0,0 +1,51 @@
+"""
+
+Minimum Add to Make Parentheses Valid
+
+A parentheses string is valid if and only if:
+
+It is the empty string,
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+
+For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+Return the minimum number of moves required to make s valid.
+
+Example 1:
+
+Input: s = "())"
+Output: 1
+
+Example 2:
+
+Input: s = "((("
+Output: 3
+
+Example 3:
+
+Input: s = "()"
+Output: 0
+
+Example 4:
+
+Input: s = "()))(("
+Output: 4
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] is either '(' or ')'.
+
+"""
+
+class Solution(object):
+    def minAddToMakeValid(self, S):
+        ans = bal = 0
+        for symbol in S:
+            bal += 1 if symbol == '(' else -1
+            # It is guaranteed bal >= -1
+            if bal == -1:
+                ans += 1
+                bal += 1
+        return ans + bal
