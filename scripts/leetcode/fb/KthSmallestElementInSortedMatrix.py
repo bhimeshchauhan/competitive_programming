2106047eeedad67fbd8920d3995b76f119b1cbba
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-21 11:42:22 +0530

    K-th-Smallest element in a matrix

--- scripts/leetcode/fb/KthSmallestElementInSortedMatrix.py
+++ scripts/leetcode/fb/KthSmallestElementInSortedMatrix.py
@@ -30,6 +30,44 @@ All the rows and columns of matrix are guaranteed to be sorted in non-decreasing
 
 """
 
+# Heap
+
+import heapq
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        
+        # The size of the matrix
+        N = len(matrix)
+        
+        # Preparing our min-heap
+        minHeap = []
+        for r in range(min(k, N)):
+            
+            # We add triplets of information for each cell
+            minHeap.append((matrix[r][0], r, 0))
+        
+        # Heapify our list
+        heapq.heapify(minHeap)    
+        
+        # Until we find k elements
+        while k:
+            
+            # Extract-Min
+            element, r, c = heapq.heappop(minHeap)
+            
+            # If we have any new elements in the current row, add them
+            if c < N - 1:
+                heapq.heappush(minHeap, (matrix[r][c+1], r, c+1))
+            
+            # Decrement k
+            k -= 1
+        
+        return element  
+
+
+# Binary Search
+
 class Solution:
     
     def countLessEqual(self, matrix, mid, smaller, larger):
