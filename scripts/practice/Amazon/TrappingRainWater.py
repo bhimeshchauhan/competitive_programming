2aae5c853241d3621dd679c17661ac30026728e2
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-23 09:15:58 +0530

    Trapping Rain Water

---
+++ scripts/practice/Amazon/TrappingRainWater.py
@@ -0,0 +1,88 @@
+"""
+
+Trapping Rain Water
+
+Given n non-negative integers representing an elevation map where the 
+width of each bar is 1, compute how much water it can trap after raining.
+
+Example 1:
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+Constraints:
+
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+
+"""
+
+"""
+
+Complextiy
+
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+
+"""
+
+
+def trap(self, bars):
+    if not bars or len(bars) < 3:
+        return 0
+    volume = 0
+    left, right = 0, len(bars) - 1
+    l_max, r_max = bars[left], bars[right]
+    while left < right:
+        l_max, r_max = max(bars[left], l_max), max(bars[right], r_max)
+        if l_max <= r_max:
+            volume += l_max - bars[left]
+            left += 1
+        else:
+            volume += r_max - bars[right]
+            right -= 1
+    return volume
+
+
+"""
+
+Complexity analysis
+
+Time complexity: O(n)O(n). Single iteration of O(n)O(n).
+Space complexity: O(1)O(1) extra space. Only constant space required 
+forleft, right, left_max and right_max.
+
+"""
+
+
+class Solution:
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        areas = 0
+        max_l = max_r = 0
+        l = 0
+        r = len(height)-1
+        while l < r:
+            if height[l] < height[r]:
+                if height[l] > max_l:
+                    max_l = height[l]
+                else:
+                    areas += max_l - height[l]
+                l += 1
+            else:
+                if height[r] > max_r:
+                    max_r = height[r]
+                else:
+                    areas += max_r - height[r]
+                r -= 1
+        return areas
