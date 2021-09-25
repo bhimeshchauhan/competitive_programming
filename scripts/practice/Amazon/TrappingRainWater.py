235a21f56a502e2fc1da972efeedbf590f30463c
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-25 21:30:41 +0530

    trapping-water

--- scripts/practice/Amazon/TrappingRainWater.py
+++ scripts/practice/Amazon/TrappingRainWater.py
@@ -30,23 +30,32 @@ n == height.length
 Complextiy
 
 Time Complexity: O(n^2)
-Space Complexity: O(n)
+Space Complexity: O(1)
 
 """
 
 
 def trap(self, bars):
+    # base case if no bars are present or are less than 3
     if not bars or len(bars) < 3:
         return 0
     volume = 0
+    # start pointers from both ends
     left, right = 0, len(bars) - 1
+    # current left and right max as the ones initiated
     l_max, r_max = bars[left], bars[right]
+    # While left and right pointers have not crossed each other
     while left < right:
+        # Get left max and right max
         l_max, r_max = max(bars[left], l_max), max(bars[right], r_max)
+        # if left max is less than equal to right max
         if l_max <= r_max:
+            # volume is on left side of the bars
             volume += l_max - bars[left]
             left += 1
+        # if left max is greater than right max
         else:
+            # volume is on the right side of the bars
             volume += r_max - bars[right]
             right -= 1
     return volume
@@ -56,9 +65,9 @@ def trap(self, bars):
 
 Complexity analysis
 
-Time complexity: O(n)O(n). Single iteration of O(n)O(n).
-Space complexity: O(1)O(1) extra space. Only constant space required 
-forleft, right, left_max and right_max.
+Time complexity: O(n). Single iteration of O(n).
+Space complexity: O(1) extra space. Only constant space required 
+for left, right, left_max and right_max.
 
 """
 
