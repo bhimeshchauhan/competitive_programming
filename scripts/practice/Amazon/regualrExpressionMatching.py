9e1d606b0087dd0d6559f99ba3256326bab621f9
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-25 20:02:51 +0530

    Regular Expression Matching

---
+++ scripts/practice/Amazon/regualrExpressionMatching.py
@@ -0,0 +1,71 @@
+"""
+
+Regular Expression Matching
+
+Given an input string s and a pattern p, implement regular expression 
+matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. 
+Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Example 4:
+
+Input: s = "aab", p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+
+Example 5:
+
+Input: s = "mississippi", p = "mis*is*p*."
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= p.length <= 30
+s contains only lowercase English letters.
+p contains only lowercase English letters, '.', and '*'.
+It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
+"""
+
+"""
+
+Time: O((T+P)2^(T+P/2)
+Space: O(T^2+P^2)
+
+"""
+
+class Solution(object):
+    def isMatch(self, text, pattern):
+        if not pattern:
+            return not text
+
+        first_match = bool(text) and pattern[0] in {text[0], '.'}
+
+        if len(pattern) >= 2 and pattern[1] == '*':
+            return (self.isMatch(text, pattern[2:]) or
+                    first_match and self.isMatch(text[1:], pattern))
+        else:
+            return first_match and self.isMatch(text[1:], pattern[1:])
