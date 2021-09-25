ac16455e010e020d70b3a02f01907c4205a70925
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-25 16:06:25 +0530

    Number of Connected Components in an Undirected Graph

---
+++ scripts/practice/Amazon/NumberofConnectedComponentsinanUndirectedGraph.py
@@ -0,0 +1,56 @@
+"""
+
+Number of Connected Components in an Undirected Graph
+
+You have a graph of n nodes. You are given an integer n and an 
+array edges where edges[i] = [ai, bi] indicates that there is an 
+edge between ai and bi in the graph.
+
+Return the number of connected components in the graph. 
+
+Example 1:
+
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+
+Example 2:
+
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= ai <= bi < n
+ai != bi
+There are no repeated edges.
+
+"""
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        count = 0
+        graph = [[] for _ in range(n)]
+        seen = [False for _ in range(n)]
+
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        def dfs(node):
+            for adj in graph[node]:
+                if not seen[adj]:
+                    seen[adj] = True
+                    dfs(adj)
+
+        for i in range(n):
+            if not seen[i]:
+                count += 1
+                seen[i] = True
+                dfs(i)
+
+        return count
