279fd6c69b178441f4ec42230156f3a641a17ad8
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-26 23:01:14 +0530

    Second Largest

---
+++ scripts/practice/FB-reRun/SecondMaxInList.py
@@ -0,0 +1,20 @@
+"""
+
+Second Largest
+
+"""
+
+
+def get_second_largest(arr):
+    second_max = float("-inf")
+    max = float("-inf")
+
+    for i in range(0, len(arr)):
+        item = int(arr[i])
+        if item > max:
+            second_max = max
+            max = item
+        elif item > second_max and item < max:
+            second_max = item
+
+    return -1 if second_max == float("-inf") else second_max
