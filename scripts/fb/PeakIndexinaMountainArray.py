8c79c36062f3d5edfdbacebc360ad8834c2b590f
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-09-26 10:21:47 +0530

    Peak Index in a Mountain Array

--- scripts/fb/PeakIndexinaMountainArray.py
+++ scripts/fb/PeakIndexinaMountainArray.py
@@ -75,7 +75,7 @@ class Solution(object):
     def peakIndexInMountainArray(self, A):
         lo, hi = 0, len(A) - 1
         while lo < hi:
-            mi = (lo + hi) / 2
+            mi = (lo + hi) // 2
             if A[mi] < A[mi + 1]:
                 lo = mi + 1
             else:
