bacc7ce162249cdd4c9a98f40fd5a48d8e7f53e1
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-10-13 22:55:05 +0530

    Merge Sorted Array

---
+++ scripts/fbOS/coding/mergeSortedList.py
@@ -0,0 +1,133 @@
+"""
+
+Merge Sorted Array
+
+You are given two integer arrays nums1 and nums2, sorted in 
+non-decreasing order, and two integers m and n, representing 
+the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but 
+instead be stored inside the array nums1. To accommodate this, 
+nums1 has a length of m + n, where the first m elements denote the 
+elements that should be merged, and the last n elements are set to 
+0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements 
+coming from nums1.
+
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only 
+there to ensure the merge result can fit in nums1.
+ 
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+
+"""
+
+# Merge and sort
+"""
+Time complexity : O((n+m)log(n+m)).
+Space complexity : O(n), but it can vary.
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Write the elements of num2 into the end of nums1.
+        for i in range(n):
+            nums1[i + m] = nums2[i]
+        
+        # Sort nums1 list in-place.
+        nums1.sort()
+        
+        
+# Three Pointers (Start From the Beginning)
+"""
+Time complexity : O(n+m).
+Space complexity : O(m).
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Make a copy of the first m elements of nums1.
+        nums1_copy = nums1[:m] 
+        
+        # Read pointers for nums1Copy and nums2 respectively.
+        p1 = 0
+        p2 = 0
+        
+        # Compare elements from nums1Copy and nums2 and write the smallest to nums1.
+        for p in range(n + m):
+            # We also need to ensure that p1 and p2 aren't over the boundaries
+            # of their respective arrays.
+            if p2 >= n or (p1 < m and nums1_copy[p1] <= nums2[p2]):
+                nums1[p] = nums1_copy[p1] 
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 += 1
+
+
+# Three Pointers (Start From the End)
+"""
+Time complexity : O(n+m).
+Space complexity : O(1).
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        # Set p1 and p2 to point to the end of their respective arrays.
+        p1 = m - 1
+        p2 = n - 1
+    
+        # And move p backwards through the array, each time writing
+        # the smallest value pointed at by p1 or p2.
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+                
+
+
