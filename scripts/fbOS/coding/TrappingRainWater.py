bc9abfe4c6c2b482b9521c396bbc87538f53b7fd
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-10-23 17:51:36 +0530

    trapping-rain-water

--- scripts/fbOS/coding/TrappingRainWater.py
+++ scripts/fbOS/coding/TrappingRainWater.py
@@ -54,4 +54,19 @@ class Solution:
 if __name__ == "__main__":
     height = [4,2,0,3,2,5]
     print(Solution().trap(height))
-        
+
+
+class Solution:
+    def trap(self, height):
+        left, right = 0, len(height) - 1
+        ans = left_max = right_max = 0
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                ans += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                ans += right_max - height[right]
+                right -= 1
+        return ans        
