a9151559ec9988a1be259dbed0e5094a4182e36f
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-10-27 23:39:54 +0530

    alien-dictionary

--- scripts/fbOS/coding/AlienDictionary.py
+++ scripts/fbOS/coding/AlienDictionary.py
@@ -54,3 +54,56 @@ words[i] consists of only lowercase English
 letters.
 
 """
+
+from collections import defaultdict, Counter, deque
+
+
+class Solution:
+    def alienOrder(self, words):
+        print(list(zip(words, words[1:])))
+        adj_list = defaultdict(set)
+        in_degree = Counter({c: 0 for word in words for c in word})
+        print('adj_list', adj_list)
+        print('in_degree', in_degree)
+
+        # Step 1: We need to populate adj_list and in_degree.
+        # For each pair of adjacent words...
+        for first_word, second_word in zip(words, words[1:]):
+            for c, d in zip(first_word, second_word):
+                if c != d:
+                    if d not in adj_list[c]:
+                        adj_list[c].add(d)
+                        in_degree[d] += 1
+                    break
+            else:  # Check that second word isn't a prefix of first word.
+                if len(second_word) < len(first_word):
+                    return ""
+
+        print('adj_list -> ', adj_list)
+        print('in_degree -> ', in_degree)
+
+        # Step 2: We need to repeatedly pick
+        # off nodes with an indegree of 0.
+        # BFS
+        output = []
+        queue = deque([c for c in in_degree if in_degree[c] == 0])
+        while queue:
+            c = queue.popleft()
+            output.append(c)
+            for d in adj_list[c]:
+                in_degree[d] -= 1
+                if in_degree[d] == 0:
+                    queue.append(d)
+
+        # If not all letters are in output, that means there was a cycle and so
+        # no valid ordering. Return "" as per the problem description.
+        if len(output) < len(in_degree):
+            return ""
+        # Otherwise, convert the ordering we found into a string and return it.
+        return "".join(output)
+
+
+if __name__ == "__main__":
+    words = ["wxqkj", "whqg", "cckgh", "cdxg", "cdxt", "cdht",
+             "ktgxt", "ktgch", "ktdw", "ktdc", "jqw", "jmc", "jmg"]
+    print(Solution().alienOrder(words))
