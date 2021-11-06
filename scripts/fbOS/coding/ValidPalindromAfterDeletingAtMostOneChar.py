a1f27c5ddfc5a73c165e9ed8660c87510ffdb600
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-07 00:57:39 +0530

    valid palindrome after removing at most one charachter
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/fbOS/coding/ValidPalindromAfterDeletingAtMostOneChar.py
@@ -0,0 +1,47 @@
+"""
+
+Valid Palindrome II
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+
+Input: s = "abc"
+Output: false
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+
+"""
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+            p1=0
+            p2=len(s)-1
+            while p1<=p2:
+                if s[p1]!=s[p2]:
+                    string1=s[:p1]+s[p1+1:]
+                    string2=s[:p2]+s[p2+1:]
+                    return string1==string1[::-1] or string2==string2[::-1]
+                p1+=1
+                p2-=1
+            return True
+        
+if __name__ == "__main__":
+    s = Solution()
+    print(s.validPalindrome("aba"))
+    print(s.validPalindrome("abca"))
+    print(s.validPalindrome("abc"))
