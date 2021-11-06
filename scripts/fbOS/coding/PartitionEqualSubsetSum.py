c22475ae645ce692bca5a9a19143caa1ba717cd6
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-06 23:31:31 +0530

    Partition Equal Subset Sum
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/fbOS/coding/PartitionEqualSubsetSum.py
+++ scripts/fbOS/coding/PartitionEqualSubsetSum.py
@@ -62,7 +62,7 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
-    # print(sol.canPartition([1, 5, 11, 5]))
-    # print(sol.canPartition([1, 2, 3, 5]))
-    # print(sol.canPartition([14,9,8,4,3,2]))
+    print(sol.canPartition([1, 5, 11, 5]))
+    print(sol.canPartition([1, 2, 3, 5]))
+    print(sol.canPartition([14,9,8,4,3,2]))
     print(sol.canPartition([6, 7, 4, 3]))
