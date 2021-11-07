238942d8453c50dd9668c28bc3b90bc6ef479c2e
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-07 23:15:49 +0530

    Remove Nth Node From End of List
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/fbOS/coding/RemoveNthNodeFromEndOfLinkedList.py
@@ -0,0 +1,59 @@
+"""
+
+Remove Nth Node From End of List
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1] 
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+
+Follow up: Could you do this in one pass?
+
+"""
+
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        fast = head
+        slow = head
+        # advance fast to nth position
+        for i in range(n):
+            fast = fast.next
+
+        if not fast:
+            return head.next
+        # then advance both fast and slow now they are nth postions apart
+        # when fast gets to None, slow will be just before the item to be deleted
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        # delete the node
+        slow.next = slow.next.next
+        return head
