0368841bbef9d0a7304f6bc2caf2f19f8c0e9ebf
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-08 13:23:33 +0530

    Missing Number
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/fbOS/coding/MissingNumber.py
@@ -0,0 +1,75 @@
+"""
+
+Missing Number
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+Example 4:
+
+Input: nums = [0]
+Output: 1
+Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+"""
+
+
+class Solution:
+    # sorting
+    def missingNumberSort(self, nums):
+        nums.sort()
+
+        # Ensure that n is at the last index
+        if nums[-1] != len(nums):
+            return len(nums)
+        # Ensure that 0 is at the first index
+        elif nums[0] != 0:
+            return 0
+
+        # If we get here, then the missing number is on the range (0, n)
+        for i in range(1, len(nums)):
+            expected_num = nums[i-1] + 1
+            if nums[i] != expected_num:
+                return expected_num
+
+    # HashSet
+
+    def missingNumberHS(self, nums):
+        num_set = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in num_set:
+                return number
+
+
+if __name__ == "__main__":
+    nums = [3, 0, 1]
+    print(Solution().missingNumberHS(nums))
+    print(Solution().missingNumberSort(nums))
