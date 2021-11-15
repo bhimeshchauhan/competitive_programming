fa57ed37bc4bf05e1ad3510158614b0bb8f1dc86
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-15 11:59:55 +0530

    Squares of a Sorted Array
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/fbOS/coding/SquaresofASortedArray.py
+++ scripts/fbOS/coding/SquaresofASortedArray.py
@@ -26,3 +26,24 @@ Follow up: Squaring each element and sorting the new array is very trivial, coul
 
 """
 
+
+class Solution:
+    def sortedSquares(self, nums):
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
+
+
+if __name__ == "__main__":
+    nums = [-4, -1, 0, 3, 10]
+    print(Solution().sortedSquares(nums))
