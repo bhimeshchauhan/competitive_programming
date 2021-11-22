a80a5f87f55ab1ee210d16fc51be9b37128c42b4
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-22 20:28:30 +0530

    Search string with Trie using Wildcard
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/fbOS/coding/searchTriwWithWildCard.py
@@ -0,0 +1,68 @@
+"""
+
+Search string with Trie using Wildcard
+
+Search a string in TRIE DS where search string can have "." and "". For example: "aB.cZ"
+
+"""
+
+
+from typing import Dict, List, Any
+
+WILDCARD = '.'
+
+
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.isLeaf = False
+
+    def insert(self, key):
+        node = self
+        for char in key:
+            if char not in node.children:
+                node.children[char] = Trie()
+            node = node.children[char]
+        node.isLeaf = True
+
+    def find(self, key):
+        stack = [(self, 0, '')]
+        result = []
+
+        while stack:
+            curr, count, currWord = stack.pop()
+
+            if count == len(key):
+                if curr.isLeaf:
+                    result.append(currWord)
+                continue
+
+            currChar = key[count]
+            if currChar == WILDCARD:
+                for childChar, node in curr.children.items():
+                    stack.append((node, count + 1, currWord + childChar))
+                continue
+
+            if currChar in curr.children:
+                node = curr.children[currChar]
+                stack.append((node, count + 1, currWord + currChar))
+
+        return result
+
+
+def main():
+    trie = Trie()
+    trie.insert('word')
+    trie.insert('ward')
+    trie.insert('oi')
+    trie.insert('boi')
+
+    assert(trie.find('??') == ['oi'])
+    assert(trie.find('') == [])
+    assert(trie.find('w?rd') == ['ward', 'word'])
+    assert(trie.find('???') == ['boi'])
+    assert(trie.find('?oi') == ['boi'])
+
+
+if __name__ == '__main__':
+    main()
