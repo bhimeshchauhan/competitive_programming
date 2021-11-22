56016a3eddb1a3481839580374383f03701adfbf
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-22 18:26:33 +0530

    Frequency of Items In subsequence of List
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/fbOS/coding/freqOfItemInList.py
+++ scripts/fbOS/coding/freqOfItemInList.py
@@ -17,29 +17,42 @@ from collections import defaultdict
 
 
 def high_freq(arr, m):
+    # Count the frequency of m elements in the array
     d = defaultdict(int)
     for i in range(m):
         d[arr[i]] += 1
+
     max_freq = max(d.values())
+    # Set the inverse frequency as {[index]: set([numbers])}
     inv_freq = defaultdict(set)
     for k, v in d.items():
         inv_freq[v].add(k)
+
+    # Get the indices of the highest frequency
     r = [max_freq]
     for i in range(len(arr)-m):
+        print(d, inv_freq)
+        # Remove the first element
         inv_freq[d[arr[i]]].remove(arr[i])
+        # decrease the frequency of the last element
         d[arr[i]] -= 1
+        # Add the new element to the inverse frequency
         inv_freq[d[arr[i]]].add(arr[i])
         if arr[i+m] in inv_freq[d[arr[i+m]]]:
             inv_freq[d[arr[i+m]]].remove(arr[i+m])
         d[arr[i+m]] += 1
+        
         inv_freq[d[arr[i+m]]].add(arr[i+m])
+
         if d[arr[i+m]] > max_freq:
             max_freq += 1
         elif len(inv_freq[max_freq]) == 0:
             max_freq -= 1
+
         r.append(max_freq)
     return r
 
 
 if __name__ == "__main__":
+    print([1, 2, 3, 1, 2, 4, 1, 4, 4])
     print(high_freq([1, 2, 3, 1, 2, 4, 1, 4, 4], 4))
