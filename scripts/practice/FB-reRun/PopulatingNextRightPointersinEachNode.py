5cb511d91da02f175b2528ab9910091374c43eb9
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-24 19:36:24 +0530

    Populating Next Right Pointers in Each Node
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/practice/FB-reRun/PopulatingNextRightPointersinEachNode.py
+++ scripts/practice/FB-reRun/PopulatingNextRightPointersinEachNode.py
@@ -5,10 +5,10 @@ Populating Next Right Pointers in Each Node
 You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
 
 struct Node {
-  int val;
-  Node *left;
-  Node *right;
-  Node *next;
+    int val;
+    Node *left;
+    Node *right;
+    Node *next;
 }
 Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
 
@@ -24,7 +24,6 @@ Example 2:
 
 Input: root = []
 Output: []
- 
 
 Constraints:
 
@@ -40,86 +39,85 @@ The recursive approach is fine. You may assume implicit stack space does not cou
 
 # Level Order Traversal
 
-import collections 
+import collections
+
 
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
-        
+    def connect(self, root):
+
         if not root:
             return root
-        
+
         # Initialize a queue data structure which contains
         # just the root of the tree
         Q = collections.deque([root])
-        
-        # Outer while loop which iterates over 
+
+        # Outer while loop which iterates over
         # each level
         while Q:
-            
+
             # Note the size of the queue
             size = len(Q)
-            
+
             # Iterate over all the nodes on the current level
             for i in range(size):
-                
+
                 # Pop a node from the front of the queue
                 node = Q.popleft()
-                
+
                 # This check is important. We don't want to
                 # establish any wrong connections. The queue will
                 # contain nodes from 2 levels at most at any
-                # point in time. This check ensures we only 
+                # point in time. This check ensures we only
                 # don't establish next pointers beyond the end
                 # of a level
                 if i < size - 1:
                     node.next = Q[0]
-                
+
                 # Add the children, if any, to the back of
                 # the queue
                 if node.left:
                     Q.append(node.left)
                 if node.right:
                     Q.append(node.right)
-        
+
         # Since the tree has now been modified, return the root node
         return root
-    
-    
 
 
 # Using previously established next pointers
 
 
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
-        
+    def connect(self, root):
+
         if not root:
             return root
-        
+
         # Start with the root node. There are no next pointers
         # that need to be set up on the first level
         leftmost = root
-        
+
         # Once we reach the final level, we are done
         while leftmost.left:
-            
+
             # Iterate the "linked list" starting from the head
-            # node and using the next pointers, establish the 
+            # node and using the next pointers, establish the
             # corresponding links for the next level
             head = leftmost
             while head:
-                
+
                 # CONNECTION 1
                 head.left.next = head.right
-                
+
                 # CONNECTION 2
                 if head.next:
                     head.right.next = head.next.left
-                
+
                 # Progress along the list (nodes on the current level)
                 head = head.next
-            
+
             # Move onto the next level
             leftmost = leftmost.left
-        
-        return root 
+
+        return root
