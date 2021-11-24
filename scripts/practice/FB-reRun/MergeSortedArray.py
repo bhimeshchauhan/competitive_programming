3e95d42c186cf2c554b77bdbfe6ca5ed24a499db
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-25 01:18:27 +0530

    Merge Sorted Array
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/practice/FB-reRun/MergeSortedArray.py
+++ scripts/practice/FB-reRun/MergeSortedArray.py
@@ -9,20 +9,20 @@ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
-
 Example 1:
 
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 Output: [1,2,2,3,5,6]
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
 Example 2:
 
 Input: nums1 = [1], m = 1, nums2 = [], n = 0
 Output: [1]
 Explanation: The arrays we are merging are [1] and [].
 The result of the merge is [1].
+
 Example 3:
 
 Input: nums1 = [0], m = 0, nums2 = [1], n = 1
@@ -30,7 +30,6 @@ Output: [1]
 Explanation: The arrays we are merging are [] and [1].
 The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
- 
 
 Constraints:
 
@@ -39,23 +38,22 @@ nums2.length == n
 0 <= m, n <= 200
 1 <= m + n <= 200
 -109 <= nums1[i], nums2[j] <= 109
- 
 
 Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 
-
 """
 
+
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m, nums2, n):
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+
         # Set p1 and p2 to point to the end of their respective arrays.
         p1 = m - 1
         p2 = n - 1
-    
+
         # And move p backwards through the array, each time writing
         # the smallest value pointed at by p1 or p2.
         for p in range(n + m - 1, -1, -1):
@@ -67,4 +65,3 @@ class Solution:
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
-                
