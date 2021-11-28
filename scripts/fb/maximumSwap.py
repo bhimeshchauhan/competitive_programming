9f892f97a00ac9109406d16c7b7a1759a51d1f94
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-29 01:17:36 +0530

    Maximum Swap digits in a number to gt Max number
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/fb/maximumSwap.py
+++ scripts/fb/maximumSwap.py
@@ -1,5 +1,5 @@
 """
-Maximum Swap
+Maximum Swap digits in a number to gt Max number
 
 You are given an integer num. You can swap two digits at most once to get the maximum valued number.
 
@@ -16,7 +16,6 @@ Example 2:
 Input: num = 9973
 Output: 9973
 Explanation: No swap.
- 
 
 Constraints:
 
@@ -25,17 +24,18 @@ Constraints:
 """
 
 class Solution:
-    def maximumSwap(self, num: int) -> int:
+    def maximumSwap(self, num):
         maxNum = num
         idx, jdx = 0, 0
         while idx < len(str(num)):
             for jdx in range(len(str(num))):
                 strNum = list(str(num))
-                print(idx, jdx, strNum[jdx], strNum[idx])
                 if(idx != jdx and strNum[jdx]>strNum[idx]):
                     strNum[idx],strNum[jdx]=strNum[jdx],strNum[idx]
-                    print(idx, jdx, strNum)
                     maxNum = max(maxNum, int(''.join(strNum)))
             idx += 1
-        print(maxNum)
         return maxNum
+    
+if __name__ == "__main__":
+    num = 2736
+    print(Solution().maximumSwap(num))
