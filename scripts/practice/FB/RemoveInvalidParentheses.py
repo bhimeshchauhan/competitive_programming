95f8f4fd949a1b3482d1c437721f65042325106c
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-29 00:31:15 +0530

    remove-invalid-paranthsis
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/practice/FB/RemoveInvalidParentheses.py
+++ scripts/practice/FB/RemoveInvalidParentheses.py
@@ -29,9 +29,11 @@ s consists of lowercase English letters and parentheses '(' and ')'.
 There will be at most 20 parentheses in s.
 """
 import collections
+
+
 class Solution:
-    def removeInvalidParentheses(self, s: str) -> List[str]:
-        
+    def removeInvalidParentheses(self, s):
+
         def is_valid(expr):
             count = 0
             for ch in expr:
@@ -43,33 +45,38 @@ class Solution:
                     if count < 0:
                         return False
             return count == 0
-        
+
         queue = collections.deque()
         queue.append(s)
-        
+
         seen = set()
         seen.add(s)
-        
+
         stay_at_this_level = False
         output = []
-        
+
         while queue:
-            
+
             expression = queue.popleft()
-            
+
             if is_valid(expression):
                 output.append(expression)
                 stay_at_this_level = True
-                
+
             elif not stay_at_this_level:
                 # populate queue with candidiates at the next level i.e. one less ( or )
                 for i in range(len(expression)):
-                    
+
                     if expression[i] in '()':
                         candidate = expression[:i] + expression[i+1:]
-                        
+
                         if candidate not in seen:
                             queue.append(candidate)
                             seen.add(candidate)
-                            
+
         return output if output else ['']
+
+
+if __name__ == '__main__':
+    s = "()())()"
+    print(Solution().removeInvalidParentheses(s))
