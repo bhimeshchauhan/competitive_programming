aae71a66fd9f59754bc7b17ea3940fc87d558677
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-28 14:58:02 +0530

    Add Strings
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/addStrings.py
+++ scripts/addStrings.py
@@ -1,11 +1,11 @@
 """
 
+Add Strings
+
 Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
 
 You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must 
 also not convert the inputs to integers directly.
-
- 
 
 Example 1:
 
@@ -21,7 +21,6 @@ Example 3:
 
 Input: num1 = "0", num2 = "0"
 Output: "0"
- 
 
 Constraints:
 
@@ -37,7 +36,7 @@ Solution:
 
 
 class Solution:
-    def addStrings(self, num1: str, num2: str) -> str:
+    def addStrings(self, num1, num2):
         # def str2int(num):
         #     numDict = {'0' : 0, '1' : 1, '2' : 2, '3' : 3, '4' : 4, '5' : 5,
         #           '6' : 6, '7' : 7, '8' : 8, '9' : 9}
--- scripts/fbOS/coding/test.py
+++ scripts/fbOS/coding/test.py
@@ -1,4 +1,7 @@
-def copyListWithRandomPointer(list):
-    if list is None:
-        return None
-    
+def addStrings(num1, num2):
+    """
+    :type num1: str
+    :type num2: str
+    :rtype: str
+    """
+    for 
