e5747172c25444bbba5c50a300ab23451e35fd1f
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-29 09:16:39 +0530

    Longest Arithmetic Subsequence
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/practice/FB/LongestArithmeticSubSequqenceWithDifference.py
+++ scripts/practice/FB/LongestArithmeticSubSequqenceWithDifference.py
@@ -36,45 +36,78 @@ Constraints:
 
 """
 
+from typing import List
+from collections import defaultdict
+
+
 class Solution:
     """
     6 June 2020.
     DP - Bottom up.
     Look at the solution as to how it was done.    
-    
+
     T: O(N^2). The use of the double for loops.
     S: O(N^2). The lengths of the dictionary in dp follows this order: 0, 1, 2, 3,...n. That's N^2.
-    
+
     **The literal running time varies with LC. This same solution ran in 2.1ms and 1.1ms.**
     **The literal space time is consistent at like 150MB which is insanely high but beats 80%. **
     """
+
     def longestArithSeqLength(self, nums: List[int]) -> int:
         # Minimum answer is always 2.
-        if len(nums) < 2: 
-            return len(A)  
-        
-        # The DP is a list of dictionaries. 
+        if len(nums) < 2:
+            return len(A)
+
+        # The DP is a list of dictionaries.
         # dp[i] is the dictionary for item i in nums
         # Each kv pair in dp[i] is delta:lengthOfSubsequence.
         n = len(nums)
         dp = [{} for i in range(n)]
         result = 2
-        
+
         for i in range(1, n):
             for j in range(i):
                 delta = nums[i] - nums[j]
-                
+
                 # If we've seen this delta with dp[j], then increase the length of the subseq by 1.
                 # This is equivalent of dp[i] 'adding on' to the subsequence.
                 if delta in dp[j]:
                     currentLength = dp[j].get(delta)
                     dp[i][delta] = currentLength + 1
-                
+
                 # Else, start a new subsequence with just dp[i] and dp[j].
                 # Length is always two.
                 else:
                     dp[i][delta] = 2
-                
+
                 # Update max.
-                result = max(result, dp[i][delta])        
+                result = max(result, dp[i][delta])
         return result
+
+
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]):
+        """ 
+        - have a `sequence_cache` hashmap for each element in the array
+            with the keys and values: `{sequence_difference: count/length}`
+        - iterate in reverse order
+        - for each `element_1`:
+            - iterate through all the elements to its right, and for each `element_2`:
+                - get the `sequence difference`: (`element_1-element_2`)
+                - check if staring a sequence with that sequence difference will be greater than what we have seen b4 for the same sequence difference
+                - update the longest var to reflect the longest we have seen so far
+        """
+        longest = 0
+        seq_cache = [defaultdict(lambda: 1) for num in nums]
+
+        for idx_1 in reversed(range(len(nums))):
+            for idx_2 in range(idx_1+1, len(nums)):
+                seq_diff = nums[idx_2] - nums[idx_1]
+
+                # current_seq_len = max(current_seq_len, seq_starting_at_idx_2_len+1)
+                seq_cache[idx_1][seq_diff] = max(
+                    seq_cache[idx_1][seq_diff], seq_cache[idx_2][seq_diff]+1)
+
+                longest = max(longest, seq_cache[idx_1][seq_diff])
+
+        return longest
