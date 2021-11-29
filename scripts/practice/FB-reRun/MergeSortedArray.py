7efcec8e668a411e5e9843c1b149ed1e6a493c6e
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-11-29 09:55:59 +0530

    Merge Sorted Array
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/practice/FB-reRun/MergeSortedArray.py
+++ scripts/practice/FB-reRun/MergeSortedArray.py
@@ -65,3 +65,12 @@ class Solution:
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
+
+
+if __name__ == '__main__':
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    Solution().merge(nums1, m, nums2, n)
+    print(nums1)
