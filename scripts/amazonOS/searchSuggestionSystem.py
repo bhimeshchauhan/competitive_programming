7910f0a827d7053994f0f7afed4dfb9e5aeddc87
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-04-24 15:51:57 +0530

    formatting
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/amazonOS/searchSuggestionSystem.py
+++ scripts/amazonOS/searchSuggestionSystem.py
@@ -2,11 +2,11 @@
 
 You are given an array of strings products and a string searchWord.
 
-Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+Design a system that suggests at most three product names from products after each character of 
+searchWord is typed. Suggested products should have common prefix with searchWord. If there are 
+more than three products with a common prefix return the three lexicographically minimums products.
 
 Return a list of lists of the suggested products after each character of searchWord is typed.
-
- 
 
 Example 1:
 
