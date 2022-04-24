09226b648b9543e305ceadb4d50c71a543bcd5e7
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-04-24 20:35:44 +0530

    merge-k-sorted-list
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/amazonOS/mergeKSortedList.py
@@ -0,0 +1,88 @@
+"""
+
+https://leetcode.com/problems/merge-k-sorted-lists/
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+
+Input: lists = []
+Output: []
+
+Example 3:
+
+Input: lists = [[]]
+Output: []
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 10^4.
+
+"""
+
+
+# Definition for singly-linked list.
+from typing import *
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not len(lists):
+            return None
+        
+        while len(lists) > 1:
+            newLists = [] 
+            for i in range(0, len(lists), 2):
+                LL1 = lists[i]
+                LL2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedList = self.merge2List(LL1, LL2)
+                newLists.append(mergedList)
+            lists = newLists
+        return lists[0]
+                
+    def merge2List(self, l1, l2):
+        dummy = ListNode()
+        current = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        if l1:
+            current.next = l1
+        if l2:
+            current.next = l2
+        return dummy.next
+
+
+if __name__ == "__main__":
+    Solution().mergeKLists(lists=[[1, 4, 5], [1, 3, 4], [2, 6]])
