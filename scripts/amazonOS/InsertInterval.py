bb5f87a24743b64fbf33e7dc6dc0f9b3c8538c9b
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-06 10:28:52 +0530

    insert-interval
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/amazonOS/InsertInterval.py
@@ -0,0 +1,68 @@
+"""
+
+Insert Interval
+
+https://leetcode.com/problems/insert-interval/
+
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Constraints:
+
+0 <= intervals.length <= 10^4
+intervals[i].length == 2
+0 <= starti <= endi <= 10^5
+intervals is sorted by starti in ascending order.
+newInterval.length == 2
+0 <= start <= end <= 10^5
+
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(intervals)):
+            # when the new interval is non overlapping and comes before the curr interval
+            if newInterval[1] < intervals[i][0]:
+                # append the new interval at start of curr interval
+                res.append(newInterval)
+                return res + intervals[i:]
+            # when the new interval is non overlapping but comes after curr interval
+            elif newInterval[0] > intervals[i][1]:
+                # will all the intervals before the new interval into result
+                # as new interval can overlap with next interval so we not add the new interval before check the overlapping case
+                res.append(intervals[i])
+            # when curr and new interval overlaps
+            else:
+                # once new interval and curr interval overlaps the new interval will have the start as min(curr_start,new_start)
+                # and end as max(curr_end,new_end)
+                newInterval = [min(newInterval[0], intervals[i][0]), max(
+                    newInterval[1], intervals[i][1])]
+
+        # once iterated over interval add the new interval into result
+        res.append(newInterval)
+        return res
+
+
+if __name__ == "__main__":
+    intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+    newInterval = [4, 8]
+    print(Solution().insert(intervals, newInterval))
