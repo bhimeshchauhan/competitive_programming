2bb249d7744d0141bd701ded8a9938b7130afe9c
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-07 14:26:31 +0530

    binary-tree-level-order-traversal
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/amazonOS/BinaryTreeLevelOrderTraversal.py
+++ scripts/amazonOS/BinaryTreeLevelOrderTraversal.py
@@ -1,6 +1,110 @@
 """
 
+Binary Tree Level Order Traversal
 
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+
+"""
+
+"""
+Time complexity : O(N) since each node is processed exactly once.
+Space complexity : O(N) to keep the output structure which contains N node values.
+"""
+
+# DFS
+
+
+
+
+from collections import deque
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        levels = []
+        if not root:
+            return levels
+
+        def helper(node, level):
+            # start the current level
+            if len(levels) == level:
+                levels.append([])
+
+            # append the current node value
+            levels[level].append(node.val)
+
+            # process child nodes for the next level
+            if node.left:
+                helper(node.left, level + 1)
+            if node.right:
+                helper(node.right, level + 1)
+
+        helper(root, 0)
+        return levels
 
 
 """
+Time complexity : O(N) since each node is processed exactly once.
+Space complexity : O(N) to keep the output structure which contains N node values.
+"""
+# BFS
+
+
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        levels = []
+        if not root:
+            return levels
+
+        level = 0
+        queue = deque([root, ])
+        while queue:
+            # start the current level
+            levels.append([])
+            # number of elements in the current level
+            level_length = len(queue)
+
+            for i in range(level_length):
+                node = queue.popleft()
+                # fulfill the current level
+                levels[level].append(node.val)
+
+                # add child nodes of the current level
+                # in the queue for the next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            # go to next level
+            level += 1
+
+        return levels
