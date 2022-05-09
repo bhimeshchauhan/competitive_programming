430cc887e842adebf9929db5492954f38b99da89
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-10 01:42:58 +0530

    sort-colors
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/amazonOS/sortColors-dutchFlag.py
@@ -0,0 +1,64 @@
+"""
+
+Sort Colors
+
+https://leetcode.com/problems/sort-colors/
+
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2. 
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # for all idx < p0 : nums[idx < p0] = 0
+        # curr is an index of element under consideration
+        p0 = curr = 0
+        # for all idx > p2 : nums[idx > p2] = 2
+        p2 = len(nums) - 1
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
+            else:
+                curr += 1
+
+
+if __name__ == "__main__":
+    nums = [2, 0, 2, 1, 1, 0]
+    Solution().sortColors(nums)
+    print(nums)
