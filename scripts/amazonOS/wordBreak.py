abf51bb5002582fb6b136111ebb686db9a67ae7b
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-14 20:05:15 +0530

    word-break
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/amazonOS/wordBreak.py
@@ -0,0 +1,53 @@
+"""
+
+Word Break
+
+https://leetcode.com/problems/word-break/
+
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+
+"""
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        @lru_cache
+        def wordBreakRecur(s: str, word_dict: FrozenSet[str], start: int):
+            if start == len(s):
+                return True
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in word_dict and wordBreakRecur(s, word_dict, end):
+                    return True
+            return False
+
+        return wordBreakRecur(s, frozenset(wordDict), 0)
