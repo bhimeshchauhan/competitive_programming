dd19fbed84c598e0928afc118f1aca19439f6c53
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-21 19:12:48 +0530

    combination-boxes
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/fb/combinationOfChickenNuggetsBoxes.py
@@ -0,0 +1,36 @@
+
+from collections import deque
+from copy import deepcopy
+
+
+class Solution:
+    def isPresent(self, q, temp):
+        lA = sorted(temp)
+        for i in q:
+            if sorted(i) == lA:
+                return True
+        return False
+
+    def combinationBoxes(self, candidates, target):
+        q = deque()
+        for item in candidates:
+            q.append([item])
+        res = []
+        while q:
+            pop = q.popleft()
+            temp = deepcopy(pop)
+            if sum(temp) == target and not self.isPresent(res, temp):
+                res.append(temp)
+            for idx in range(len(candidates)):
+                curr = candidates[idx]
+                if sum(pop)+curr <= target:
+                    temp = deepcopy(pop)
+                    temp.append(curr)
+                    q.append(temp)
+        return res
+
+
+if __name__ == "__main__":
+    candidates = [6, 10, 15]
+    target = 46
+    print(Solution().combinationBoxes(candidates, target))
