37cf3d51a3013f048fe6ed4f426e02ad0ac25b6b
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-23 17:46:36 +0530

    countBinarySubstrings
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/practice/Amazon/CountBinarySubString.py
+++ scripts/practice/Amazon/CountBinarySubString.py
@@ -33,14 +33,14 @@ s[i] is either '0' or '1'.
 class Solution(object):
     def countBinarySubstrings(self, s):
         groups = [1]
-        for i in xrange(1, len(s)):
+        for i in range(1, len(s)):
             if s[i-1] != s[i]:
                 groups.append(1)
             else:
                 groups[-1] += 1
 
         ans = 0
-        for i in xrange(1, len(groups)):
+        for i in range(1, len(groups)):
             ans += min(groups[i-1], groups[i])
         return ans
 
