3924cfe975bf4eefa2b22ee83bb963a3c7141f18
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-23 18:21:08 +0530

    kth-factor-of-N
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- scripts/practice/Amazon/KthFactorofN.py
+++ scripts/practice/Amazon/KthFactorofN.py
@@ -49,6 +49,17 @@ Constraints:
 
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
+        count = 0
+        for i in range(1, n+1):
+            if n % i == 0:
+                count += 1
+            if count == k:
+                return i
+        return -1
+
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
         for x in range(1, n // 2 + 1):
             if n % x == 0:
                 k -= 1
