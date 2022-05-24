f0b2906a15d926b92b4614bb70907c2965cd1442
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-05-24 18:15:33 +0530

    get_Card
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ scripts/practice/Amazon/get_card.py
@@ -0,0 +1,33 @@
+"""
+
+Pile of Cards
+
+
+
+"""
+
+import math
+
+def Card(self, id):
+    self.id = id
+
+    def getId(self):
+        return self.id
+
+
+def PileOfCards(self):
+    
+    def __init__(self):
+        indicesByCardId = {}
+        cads = []
+        random = math.random()
+
+    def insert(self):
+        pass
+
+    def get(self):
+        pass
+
+    def getRandom(self):
+        pass
+
